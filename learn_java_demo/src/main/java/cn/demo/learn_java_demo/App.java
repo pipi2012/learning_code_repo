74a65f86efaef8b3c5d0cb7d8dbc2bfc80aa7aac
pipi2012 <pengxing0829@163.com>
2017-02-20 10:22:52 +0800

    learn_java_demo init

---
+++ learn_java_demo/src/main/java/cn/demo/learn_java_demo/App.java
@@ -0,0 +1,13 @@
+package cn.demo.learn_java_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
